95f975bbf025109440a5d19ff318a0e9d06ee37d
nika7724 <nika0147@stud.kea.dk>
2022-05-16 11:16:32 +0200

    Initial commit

---
+++ src/main/java/com/example/nordic_motorhome_project/NordicMotorhomeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.nordic_motorhome_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordicMotorhomeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NordicMotorhomeProjectApplication.class, args);
+    }
+
+}
