6d4d4a835ea278a3a1a702daea0d78739d380878
nika7724 <nika0147@stud.kea.dk>
2022-05-16 11:42:14 +0200

    Control class

---
+++ src/main/java/com/example/nordic_motorhome_project/model/User.java
@@ -0,0 +1,4 @@
+package com.example.nordic_motorhome_project.model;
+
+public class User {
+}
---
+++ src/main/java/com/example/nordic_motorhome_project/services/UserServices.java
@@ -0,0 +1,4 @@
+package com.example.nordic_motorhome_project.services;
+
+public class UserServices {
+}
